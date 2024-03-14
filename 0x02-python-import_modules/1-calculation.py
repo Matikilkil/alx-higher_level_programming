2d06317932ab52d2bad04203ca74f7c22af2c44e
Your Name <you@example.com>
2024-03-14 02:55:32 -0700

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    """print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
@@ -12,4 +15,4 @@ result_div = div(a, b)
 print("{} + {} = {}".format(a, b, result_add))
 print("{} - {} = {}".format(a, b, result_sub))
 print("{} * {} = {}".format(a, b, result_mul))
-print("{} / {} = {}".format(a, b, result_div)
+print("{} / {} = {}".format(a, b, result_div))

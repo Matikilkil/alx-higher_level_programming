d0b80428f8d459bddbf21541275a2de1cdf1a1ff
Matikilkil <lydiahjerotichkipchumba@gmail.com>
2024-08-26 04:30:32 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
 
-def fetch_status():
-    """Fetches the status from the specified URL and displays the response body."""
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        
-    utf8_content = body.decode('utf-8')
-    
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {utf8_content}")
 
 if __name__ == "__main__":
-    fetch_status()
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
